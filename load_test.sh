e641198682f581b38949fea5716e56d3a308a524
Preedee Ponchevin <preedee.digital@gmail.com>
2024-07-04 18:56:48 +0700

    Fixed for log tracing in application insights

--- load_test.sh
+++ load_test.sh
@@ -1,1 +1,1 @@
-curl -v  -O --limit-rate 1M  http://localhost:8888/testdata/temp_5GB_file.dat --progress-bar
+curl -v  -O --limit-rate 1M  http://localhost:8888/testdata/mockfile_30mb.txt --progress-bar
--- src/blob_proxy.rs
+++ src/blob_proxy.rs
@@ -1,7 +1,4 @@
 use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::thread;
-use std::time::Duration;
 
 use actix_web::{HttpResponse, Responder, web};
 use actix_web_opentelemetry::RequestTracing;
@@ -9,13 +6,19 @@ use azure_storage::prelude::*;
 use azure_storage_blobs::prelude::*;
 use futures::stream::StreamExt;
 use futures::TryStreamExt;
-use log::{error, info};
+use log::{error, info, Level};
 use opentelemetry::KeyValue;
-use opentelemetry::trace::Tracer as _;
+use opentelemetry_appender_log::OpenTelemetryLogBridge;
+use opentelemetry_sdk::logs::{BatchLogProcessor, LoggerProvider};
+//use opentelemetry::trace::Tracer as _;
 use opentelemetry_sdk::Resource;
 use opentelemetry_sdk::runtime::Tokio;
 use opentelemetry_semantic_conventions as semcov;
 use sysinfo::System;
+
+//use std::sync::atomic::{AtomicBool, Ordering};
+//use std::thread;
+//use std::time::Duration;
 
 async fn stream_blob(path: web::Path<(String, String)>) -> impl Responder {
     let (container, blob) = path.into_inner();
@@ -77,9 +80,9 @@ async fn get_blob_client(
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    pretty_env_logger::init();
+    //pretty_env_logger::init();
 
-    info!("Starting server");
+
 
     let client = reqwest::Client::new();
 
@@ -95,21 +98,23 @@ async fn main() -> std::io::Result<()> {
         connection_string,
         client,
     ).expect("connection string is valid");
-    let logger_provider = opentelemetry_sdk::logs::LoggerProvider::builder()
-        .with_batch_exporter(exporter, Tokio)
+    let logger_provider = LoggerProvider::builder()
+        .with_log_processor(BatchLogProcessor::builder(exporter, Tokio).build())
         .with_config(
             opentelemetry_sdk::logs::config().with_resource(Resource::new(vec![
-                KeyValue::new(semcov::resource::SERVICE_NAMESPACE, "myapp"),
+                KeyValue::new(semcov::resource::SERVICE_NAMESPACE, "blob_proxy_dev"),
                 KeyValue::new(semcov::resource::SERVICE_NAME, "blob_proxy"),
             ])),
         )
         .build();
 
     let otel_log_appender =
-        opentelemetry_appender_log::OpenTelemetryLogBridge::new(&logger_provider);
+        OpenTelemetryLogBridge::new(&logger_provider);
     log::set_boxed_logger(Box::new(otel_log_appender)).expect("Could not set logger");
-    log::set_max_level(log::Level::Info.to_level_filter());
+    log::set_max_level(Level::Info.to_level_filter());
 
+
+    info!("Starting server");
 
     let mut system = System::new_all();
 
@@ -124,6 +129,7 @@ async fn main() -> std::io::Result<()> {
     );
 
 
+    /*
     // Create an atomic flag to signal thread termination
     let stop_flag = Arc::new(AtomicBool::new(false));
 
@@ -133,9 +139,9 @@ async fn main() -> std::io::Result<()> {
 
     thread::spawn(move || loop {
         if stop_flag_clone.load(Ordering::Relaxed) {
+            info!("Shutting down");
             break;
         }
-
         system.refresh_all();
         thread::sleep(Duration::from_secs(1));
         info!(
@@ -144,8 +150,7 @@ async fn main() -> std::io::Result<()> {
             (system.free_memory() as f64) / (1024 * 1024) as f64
         );
     });
-
-
+    */
     let _result = actix_web::HttpServer::new(|| {
         actix_web::App::new()
             .app_data(web::PayloadConfig::new(usize::MAX)) // Increase payload limit
@@ -158,14 +163,14 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await;
 
+
+    //info!("Stopping thread monitoring");
+    // Set the flag to stop the thread
+    //stop_flag.store(true, Ordering::Relaxed);
     info!("Server stopped");
 
     logger_provider.shutdown().unwrap();
     opentelemetry::global::shutdown_tracer_provider();
-
-    // Set the flag to stop the thread
-    stop_flag.store(true, Ordering::Relaxed);
-
 
     Ok(())
 }
